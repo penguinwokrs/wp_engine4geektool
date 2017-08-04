eef356abc2d0470f7ff4ab10fb62a1e975165ffc
penguinwokrs <dev.and.penguin@gmail.com>
2017-08-04 23:39:11 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# wp_engine4geektool
+repeating youtube video for geektool to realize wallpaper engine on mac
+
+## How to use
+1. setting geektool. (please refer to http://qiita.com/natmark/items/43f151ae663057a1883e)
+2. clone this repos.
+3. set vid in video.js
+4. set local folder's path at geektool's property url
+```
+var vid = 'SlRfwbD2riE'; //please set youtube video's id that you want to play
+```
---
+++ video.js
@@ -0,0 +1,34 @@
+var w = window, player;
+// 再生したいyoutubeの動画id
+var vid = 'UVX0OUO9ptU';
+
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+w.onYouTubeIframeAPIReady = function() {
+  player = new YT.Player('video', { // 置き換えるHTML要素のid
+    videoId: vid,
+    playerVars: {
+      rel: 0, //動画再生後の関連動画を表示しない
+      showinfo: 0, //動画再生前に動画情報を表示しない
+      autoplay: 1, //動画準備後に自動再生
+      loop: 1 //ループ設定
+    },
+    events: {
+      onStateChange: function(e) {
+        // 再生終了後
+        if ( e.data == YT.PlayerState.ENDED ) {
+	        //再生
+         	e.target.playVideo();
+        }
+      },
+	'onReady': onPlayerReady
+    }
+  });
+  player.setLoop(true);
+};
+function onPlayerReady(event) {
+	  event.target.setVolume(0);
+}
